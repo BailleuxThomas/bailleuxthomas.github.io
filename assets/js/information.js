e9b6b15c9565c63b0d6f38637d82a75599a30907
Bailleux Thomas <thomasbailleux2@gmail.com>
2020-04-25 21:24:55 +0200

    remove console.log assets/js/

--- assets/js/information.js
+++ assets/js/information.js
@@ -6,34 +6,49 @@ const img3 = document.getElementById("Img3");
 let test = 0;
 const br = "<br>";
 
-
 let Information = {
-  "Info_to_portefolio": [
-    { "Nom": "Website Beltek", "Description": "Migrer le site beltek en WordPres, améliorer le visu, le rendre plus fluide.", "Skill": "Php, Javascript, WordPress, Elementor", "Img": "./assets/img/beltek.jpg" },
-    { "Nom": "Website Ladou", "Description": "Site Ladou 'L'attiéké déshydraté'", "Skill": "WordPress, E-Commerce, Elementor", "Img": "./assets/img/ladou.png" },
-    { "Nom": "PlateFormeHorus", "Description": "La platformeHorus consiste à aider via des services de formation que cela soit en tutoriels, vidéos, chat, questions réponses.", "Skill": "WordPress,E-Commerce,Elementor,Plugin Cloud, FTP clients crées par un collègue et moi même.", "Img": "./assets/img/plateformehorus.png" },
-  ]
-}
+  Info_to_portefolio: [
+    {
+      Nom: "Website Beltek",
+      Description:
+        "Migrer le site beltek en WordPres, améliorer le visu, le rendre plus fluide.",
+      Skill: "Php, Javascript, WordPress, Elementor",
+      Img: "./assets/img/beltek.jpg",
+    },
+    {
+      Nom: "Website Ladou",
+      Description: "Site Ladou 'L'attiéké déshydraté'",
+      Skill: "WordPress, E-Commerce, Elementor",
+      Img: "./assets/img/ladou.png",
+    },
+    {
+      Nom: "PlateFormeHorus",
+      Description:
+        "La platformeHorus consiste à aider via des services de formation que cela soit en tutoriels, vidéos, chat, questions réponses.",
+      Skill:
+        "WordPress,E-Commerce,Elementor,Plugin Cloud, FTP clients crées par un collègue et moi même.",
+      Img: "./assets/img/plateformehorus.png",
+    },
+  ],
+};
 
 // console.log(Information.Info_to_portefolio.length);
 
-
 function btnleft() {
-
-
   test += 1;
 
   if (test >= Information.Info_to_portefolio.length) {
     test = 0;
   }
 
-  console.log(test);
+  // console.log(test);
 
-  console.log(Information.Info_to_portefolio[test]);
+  // console.log(Information.Info_to_portefolio[test]);
 
   demo.innerHTML = "Nom : " + Information.Info_to_portefolio[test].Nom;
   demo.innerHTML += br;
-  demo.innerHTML += "Description : " + Information.Info_to_portefolio[test].Description;
+  demo.innerHTML +=
+    "Description : " + Information.Info_to_portefolio[test].Description;
   demo.innerHTML += br;
   demo.innerHTML += "Skill : " + Information.Info_to_portefolio[test].Skill;
   demo.innerHTML += br;
@@ -44,18 +59,18 @@ function btnleft() {
 }
 
 function btnright() {
-
   test -= 1;
 
   if (test <= -1) {
-    test = Information.Info_to_portefolio.length-1;
+    test = Information.Info_to_portefolio.length - 1;
   }
 
-  console.log(test);
+  // console.log(test);
 
   demo.innerHTML = "Nom : " + Information.Info_to_portefolio[test].Nom;
   demo.innerHTML += br;
-  demo.innerHTML += "Description : " + Information.Info_to_portefolio[test].Description;
+  demo.innerHTML +=
+    "Description : " + Information.Info_to_portefolio[test].Description;
   demo.innerHTML += br;
   demo.innerHTML += "Skill : " + Information.Info_to_portefolio[test].Skill;
   demo.innerHTML += br;
